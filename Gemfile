2d9a0b5e6d1fbcd2d7e5be912b785434154209b4
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-16 00:57:25 +0800

    seeds added

--- Gemfile
+++ Gemfile
@@ -1,30 +1,32 @@
-source 'https://rubygems.org'
-ruby '2.3.5'
+source "https://rubygems.org"
+ruby "2.3.5"
 
-gem 'bootsnap', require: false
-gem 'devise'
-gem 'jbuilder', '~> 2.0'
-gem 'pg', '~> 0.21'
-gem 'puma'
-gem 'rails', '5.2.2'
-gem 'redis'
+gem "bootsnap", require: false
+gem "devise"
+gem "jbuilder", "~> 2.0"
+gem "pg", "~> 0.21"
+gem "puma"
+gem "rails", "5.2.2"
+gem "redis"
 
-gem 'autoprefixer-rails'
-gem 'font-awesome-sass', '~> 5.6.1'
-gem 'sassc-rails'
-gem 'simple_form'
-gem 'uglifier'
-gem 'webpacker'
+gem "autoprefixer-rails"
+gem "font-awesome-sass", "~> 5.6.1"
+gem "sassc-rails"
+gem "simple_form"
+gem "uglifier"
+gem "webpacker"
+
+gem "faker"
 
 group :development do
-  gem 'web-console', '>= 3.3.0'
+  gem "web-console", ">= 3.3.0"
 end
 
 group :development, :test do
-  gem 'pry-byebug'
-  gem 'pry-rails'
-  gem 'listen', '~> 3.0.5'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'dotenv-rails'
+  gem "pry-byebug"
+  gem "pry-rails"
+  gem "listen", "~> 3.0.5"
+  gem "spring"
+  gem "spring-watcher-listen", "~> 2.0.0"
+  gem "dotenv-rails"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Message.destroy_all && User.destroy_all && Channel.destroy_all
+
+nicknames = ["Britta", "Abed", "Jeff", "Annie", "Troy", "Shirley", "Pierce", "Dean"]
+users = nicknames.each { |nickname| User.create(email: "#{nickname.downcase}@community.org", password: "community")}
+
+puts "Users created!"
+
+names = ["general", "react", "london"]
+channels = names.each { |name| Channel.create(name: name) }
+
+puts "Channels created!"
+
+20.times do
+  Message.create(user_id: rand(users.length), channel_id: rand(channels.length), content: Faker::TvShows::Community.quotes)
+end
+
+puts "Messages created!"
+
+# names = %w(general paris react)
+# nicknames = %w(Papillard ssaunier monsieurpaillard krokrob Eschults)
+
+# channels = names.map do |name|
+#   Channel.find_or_create_by(name: name)
+# end
+
+# users = nicknames.map do |nickname|
+#   User.create(email: "#{nickname.downcase}@lewagon.com", password: "testtest")
+# end
+
+# 20.times do
+#   Message.create! user: users.sample, channel: channels.sample, content: Faker::TvShows::HowIMetYourMother.quote
+# end
+
+# puts "Seeds created!"
