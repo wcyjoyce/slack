f9bdbf18a583ba37ad2dab106b779b590011a8f4
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-20 06:51:56 +0800

    URLs udpated, csrfToken added, selectChannel and postMessage functions created

--- app/javascript/slack/actions/index.js
+++ app/javascript/slack/actions/index.js
@@ -2,8 +2,8 @@
 const base_url = "api/v1";
 
 export function fetchMessages(channel) {
-  const url = `${base_url}/${channel}/messages`;
-  const promise = fetch(url, { credentials: "same-origin"}).then(response => response.json());
+  const url = `${base_url}/channels/${channel}/messages`;
+  const promise = fetch(url, { credentials: "same-origin" }).then(response => response.json());
 
   return {
     type: "FETCH_MESSAGES",
@@ -11,14 +11,16 @@ export function fetchMessages(channel) {
   };
 }
 
-export function createMessage(channel, author, content) {
-  const url = `${base_url}/${channel}/messages`;
-  const body = { author, content };
+export function createMessage(channel, content) {
+  const url = `${base_url}/channels/${channel}/messages`;
+  const body = { content };
+  const csrfToken = document.querySelector("meta[name='csrf-token']").attributes.content.value;
   const promise = fetch(url, {
     method: "POST",
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "X-CSRF-Token": csrfToken
     },
     credentials: "same-origin",
     body: JSON.stringify(body)
@@ -28,4 +30,17 @@ export function createMessage(channel, author, content) {
     type: "POST_MESSAGE",
     payload: promise
   };
+}
+
+export function selectChannel() {
+  return {
+    type: "SELECT_CHANNEL"
+  }
+}
+
+export function postMessage(message) {
+  return {
+    type: "POST_MESSAGE",
+    payload: message
+  }
 }
