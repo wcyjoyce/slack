2f43f7518c51ddebc80adf91bfd747cf5a8da67f
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-22 06:39:45 +0800

    index and create methods done in messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,11 +3,15 @@ class Api::V1::MessagesController < ApplicationController
   before_action :set_channel
 
   def index
-    @messages = Message.all
+    @messages = @channel.messages.order("created_at ASC")
     render json: @messages
   end
 
   def create
+    @message = @channel.messages.build(content: params[:content])
+    @message.user = current_user
+    @message.save
+    render json: @message
   end
 
   private
