f16929699bb9239488bb51d946de1c746fe6295c
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-25 22:30:14 +0800

    routes updated

--- app/javascript/slack/index.jsx
+++ app/javascript/slack/index.jsx
@@ -1,42 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { logger } from "redux-logger";
 import reduxPromise from "redux-promise";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import { createHistory as history } from "history";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import App from "./components/app";
-
 import messagesReducer from "./reducers/messages_reducer.js";
-
-const identityReducer = (state = null) => state;
 
 const initialState = {
   messages: [],
-  channels: ["general", "react", "london"],
-  currentUser: "Joyce"
-  // currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
+  // channels: ["general", "react", "london"],
+  channels: JSON.parse(chatContainer.dataset.channels).map(channel => channel.name)
+  // currentUser: "Joyce"
 };
 
 const reducers = combineReducers({
   messages: messagesReducer,
-  channels: identityReducer,
-  currentUser: identityReducer
+  channels: (state = null, action) => state
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = composeEnhancers(applyMiddleware(reduxPromise, logger));
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, initialState, middlewares)}>
-    <Router history={history}>
+  <Provider store={store}>
+    <BrowserRouter>
       <Switch>
-        <Route path="/:channel" component={App} />
-        <Redirect from="/" to="/general" />
+        <Route path="/channels/:channel" component={App} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  // document.getElementById("root")
+  document.getElementById("chat_app")
 );
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
